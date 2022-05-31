a573649de1df683786ad8afe5e70149bb51c81f7
ekunish <ekunish@gmail.com>
2022-05-31 16:56:09 +0900

    neovim transparent

---
+++ .config/nvim/lua/user/transparent.lua
@@ -0,0 +1,12 @@
+local status_ok, transparent = pcall(require, "transparent")
+if not status_ok then
+  return
+end
+
+transparent.setup({
+  enable = false, -- boolean: enable transparent
+  extra_groups = { -- table/string: additional groups that should be cleared
+    -- In particular, when you set it to 'all', that means all available groups
+  },
+  exclude = {}, -- table: groups you don't want to clear
+})
